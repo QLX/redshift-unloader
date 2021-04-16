b2e91858a6721ff7c696c945f8c691209e5eae03
qlxjrf <49995000+qlxjrf@users.noreply.github.com>
2021-04-16 15:41:29 -0400

    Update s3.py

--- redshift_unloader/s3.py
+++ redshift_unloader/s3.py
@@ -1,11 +1,10 @@
-import boto3
-import boto3.resources
 import urllib.parse
-
 from typing import List
 
-from redshift_unloader.credential import Credential
+import boto3
+import boto3.resources
 from redshift_unloader.logger import logger
+from redshift_unloader.credential import Credential
 
 MAX_DELETE_OBJECTS = 1000
 
@@ -14,6 +13,7 @@ class S3:
     __session: boto3.session.Session
     __s3: 'boto3.resources.factory.s3.ServiceResource'
     __bucket: 'boto3.resources.factory.s3.Bucket'
+    __client: 'bott3.client'
 
     def __init__(self, credential: Credential, bucket: str, region: str) -> None:
         self.__session = boto3.session.Session(
@@ -23,6 +23,7 @@ class S3:
         )
         self.__s3 = self.__session.resource('s3')
         self.__bucket = self.__s3.Bucket(bucket)
+        self.__client = boto3.client('s3')
 
     def __del__(self) -> None:
         pass
@@ -42,3 +43,9 @@ class S3:
     def download(self, key: str, filename: str) -> None:
         logger.debug("Download %s to %s", key, filename)
         self.__bucket.download_file(Key=key, Filename=filename)
+
+    def download(self, key: str):
+        s3 = boto3.client('s3')
+        s3obj = s3.get_object(Bucket=self.__bucket.name, Key=key)
+        return s3obj['Body'].read()
+#        return self.__client.get_object(self.__bucket.name, key)['Body'].read()
